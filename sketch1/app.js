a4b50abc46de1674c5ab91690e3927f5c020ff17
geocontrol <mark@geekyoto.com>
2013-12-08 22:30:04 +0000

    Added sketch 1, a simple node app to monitor twitter. Initial code.

---
+++ sketch1/app.js
@@ -0,0 +1,9 @@
+var twitter = require('ntwitter');
+
+var twit = new twitter({
+  consumer_key: 'Twitter',
+  consumer_secret: 'API',
+  access_token_key: 'keys',
+  access_token_secret: 'go here'
+});
+
