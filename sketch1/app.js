9b54185eedd6ba898a2af076a50c074be39eb1b2
geocontrol <mark@geekyoto.com>
2013-12-08 23:17:57 +0000

    Added stream information for dronestream feed. Currently running the script will open the stream and log to the console when a tweet has been posted by the user ID being followed. In this case the ID is 904137332 which is the ID for the dronestream account.

--- sketch1/app.js
+++ sketch1/app.js
@@ -1,9 +1,24 @@
 var twitter = require('ntwitter');
 
 var twit = new twitter({
-  consumer_key: 'Twitter',
-  consumer_secret: 'API',
-  access_token_key: 'keys',
-  access_token_secret: 'go here'
+  consumer_key: '4O1kSBVL1yafz4VAWF6Hw',
+  consumer_secret: 'neg1SGatR6L6Zc9SnJG1HNpXEt3GAh9TA2IsHxw',
+  access_token_key: '5791-Cm4btdK1W8lJJohhL2ItlhqSmBwDGJaIzdf4Xy5rPoAyP',
+  access_token_secret: 'o5nKJ6Ax1KuudW0bP7pukL4okfe83ZHi7KkT8Tc2ukybM'
+});
+
+twit.stream('statuses/filter', {'follow':'904137332'}, function(stream) {
+//twit.stream('statuses/filter', {'follow':'45341566'}, function(stream) {
+  stream.on('data', function (data) {
+    console.log(data);
+  });
+  stream.on('end', function (response) {
+    // Handle a disconnection
+  });
+  stream.on('destroy', function (response) {
+    // Handle a 'silent' disconnection from Twitter, no end/error event fired
+  });
+  // Disconnect stream after five seconds
+  //setTimeout(stream.destroy, 1000);
 });
 
