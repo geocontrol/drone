5d1688bc891743d0cffa28e91438ac6dd159f39e
geocontrol <mark@geekyoto.com>
2013-12-09 21:16:03 +0000

    added code to play Cathal's mp3 demo as well as a .wav file when a tweet occurs from the target.

--- sketch1/app.js
+++ sketch1/app.js
@@ -1,5 +1,9 @@
 var twitter = require('ntwitter');
-
+var play = require('play');
+var fs = require("fs");
+var lame = require("lame");
+var Speaker = require("speaker");
+	
 var twit = new twitter({
   consumer_key: '4O1kSBVL1yafz4VAWF6Hw',
   consumer_secret: 'neg1SGatR6L6Zc9SnJG1HNpXEt3GAh9TA2IsHxw',
@@ -7,10 +11,18 @@ var twit = new twitter({
   access_token_secret: 'o5nKJ6Ax1KuudW0bP7pukL4okfe83ZHi7KkT8Tc2ukybM'
 });
 
-twit.stream('statuses/filter', {'follow':'904137332'}, function(stream) {
-//twit.stream('statuses/filter', {'follow':'45341566'}, function(stream) {
+fs.createReadStream('./sounds/drone_demo_01.mp3')
+  .pipe(new lame.Decoder)
+  .on('format', console.log)
+  .pipe(new Speaker);
+
+//twit.stream('statuses/filter', {'follow':'904137332'}, function(stream) {
+twit.stream('statuses/filter', {'follow':'5791'}, function(stream) {
   stream.on('data', function (data) {
     console.log(data);
+	play.sound('./sounds/ky57.wav', function(){
+		console.log('Playing Interrupt Sound');
+	});
   });
   stream.on('end', function (response) {
     // Handle a disconnection
